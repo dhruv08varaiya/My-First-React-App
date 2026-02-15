ec543c2e52421ce78f51848384f6eb33b530a9fb
dhruv08varaiya <dhruv.varaiya@gmail.com>
2026-02-15 23:09:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>My First React App </h1>
+      <p>this is my first React application!</p>
+    </>
+  )
+}
+
+export default App
